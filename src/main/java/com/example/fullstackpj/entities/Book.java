f5714adaf8961e417e9d3fffffa4dc84dedf4ad3
Mykyta <sayron.arnorius@yahoo.com>
2023-04-20 15:41:01 +0200

    add Book to the commit

---
+++ src/main/java/com/example/fullstackpj/entities/Book.java
@@ -0,0 +1,67 @@
+package com.example.fullstackpj.entities;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.util.Date;
+
+@Entity
+public class Book {
+
+    @Id
+    int id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    User user;
+
+    @ManyToOne
+    @JoinColumn(name = "car_id")
+    Car car;
+
+    Date startDate;
+
+    Date endDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+}
