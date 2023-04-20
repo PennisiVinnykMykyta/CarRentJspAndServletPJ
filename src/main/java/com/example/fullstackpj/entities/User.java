22de08594f8ec2b74eddd894f8f49926793149ec
Mykyta <sayron.arnorius@yahoo.com>
2023-04-20 15:40:32 +0200

    feat: created new class Book

--- src/main/java/com/example/fullstackpj/entities/User.java
+++ src/main/java/com/example/fullstackpj/entities/User.java
@@ -1,7 +1,12 @@
 package com.example.fullstackpj.entities;
 
+import com.example.fullstackpj.entities.enums.UserType;
+
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 public class User implements  Serializable{
@@ -17,20 +22,24 @@ public class User implements  Serializable{
     private String lastName;
 
     @Column(name ="user_type")
-    private String userType;
+    private UserType type;
 
-    public String getUserType(){ return userType;}
+    @Column(name = "birth_date")
+    private Date birthDate;
 
-    public void setUserType(String type){ this.userType = type;}
 
-    public String getFirstName(){ return firstName;}
+    @OneToMany(mappedBy = "user")
+    private List<Book> bookings;
 
-    public void setFirstName(String name){ this.firstName = name;}
+    public User(int id, String firstName, String lastName, UserType type, Date birthDate) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.type = type;
+        this.birthDate = birthDate;
+    }
 
-    public String getLastName(){ return lastName;}
-
-    public void setLastName(String name){ this.lastName = name;}
-
+    public  User(){}
 
     public int getId() {
         return id;
@@ -40,10 +49,35 @@ public class User implements  Serializable{
         this.id = id;
     }
 
-    public User(){}
-
-    public User(int id){
-        this.id =id;
+    public String getFirstName() {
+        return firstName;
     }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public UserType getType() {
+        return type;
+    }
+
+    public void setType(UserType type) {
+        this.type = type;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
 }
